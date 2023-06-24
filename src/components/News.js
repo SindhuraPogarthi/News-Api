4e15b1f6fa79fed069c1c5f576e1ef028b3d2553
“Sindhura <sindhurapogarthi@gmail.com>
2023-06-24 22:56:58 +0530

    updated apikey

--- src/components/News.js
+++ src/components/News.js
@@ -6,7 +6,7 @@ import './News.css'
 export default function News() {
    const[news,setnews] = useState([])
    const getnews=async()=>{
-     let data = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2023-05-24&sortBy=publishedAt&apiKey=90d4109c-af97-409a-9ab6-63a694cd6083")
+     let data = await fetch("https://newsapi.org/v2/everything?q=tesla&from=2023-05-24&sortBy=publishedAt&apiKey=de83f4a15ba6463ba195f2de88fa2ef7")
      const jsondata = await data.json()
      setnews(jsondata.articles)
      console.log(news)
